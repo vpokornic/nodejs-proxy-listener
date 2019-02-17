f16e1f2ff8c5f44f6f83652961f91c3cf45bab8c
Veran Pokornić <veran.pokornic@gmail.com>
2019-02-17 13:15:20 +0100

    Added time and IP logging of request

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
 const http = require('http')
 const port = 3000
 
-const requestHandler = (request, response) => {
-  console.log(request.url)
-  response.end('Hello Node.js Server!')
+const requestHandler = (req, res) => {
+    var ip = req.headers['x-forwarded-for'] || 
+     req.connection.remoteAddress || 
+     req.socket.remoteAddress ||
+     (req.connection.socket ? req.connection.socket.remoteAddress : null);
+
+    var msg = '[REQ] ' + new Date().toString().replace(/T/, ':').replace(/\.\w*/, '') + '\n' + ip + '\n';
+
+    console.log(msg);
+    res.end('Hello Node.js Server!\n' + msg);
 }
 
 const server = http.createServer(requestHandler)
